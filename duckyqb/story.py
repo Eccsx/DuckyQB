40a6d9e0b380d15abd0b4d15f2a5e38259086806
Thomas Mattone <thomasmattone.perso@gmail.com>
2022-10-26 15:42:46 +0200

    linting

--- duckyqb/story.py
+++ duckyqb/story.py
@@ -2,13 +2,14 @@ from duckyqb import env
 import pygame
 import sys
 
+
 def begins():
     env.create()
-    
+
+
 def ends():
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-         
